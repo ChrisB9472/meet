e52f123c5cf2664f7cfeb002a713bb0af5e2f17e
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-25 11:06:24 -0500

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
-  state = {
-
-    query: '',
-    suggestions: [],
-    showSuggestions: undefined
-  }
+    state = {
+      query: '',
+      suggestions: [],
+      showSuggestions: undefined
+    }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
@@ -21,7 +20,8 @@ class CitySearch extends Component {
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+      query: suggestion,
+      showSuggestions: false
     });
   
     this.props.updateEvents(suggestion);
@@ -30,13 +30,14 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
-        <input
-          type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-        />
-       <ul className="suggestions">
+       <input
+  type="text"
+  className="city"
+  value={this.state.query}
+  onChange={this.handleInputChanged}
+  onFocus={() => { this.setState({ showSuggestions: true }) }}
+/>
+<ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
   {this.state.suggestions.map((suggestion) => (
     <li
       key={suggestion}
