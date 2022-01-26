16c5264773701cfcfe82f9152a568a4b38ec5eaf
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-26 10:55:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('2e2ccb34428f4f4fb4db9660bd214103').install();
 
 ReactDOM.render(
   <React.StrictMode>
