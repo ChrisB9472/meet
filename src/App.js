5bc957e237be7d6ac4d97c82bf65baddcf8c4bf9
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-26 11:29:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import { getEvents, extractLocations } from './api';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   state = {
     events: [],
-    locations: []
+    locations: [],
+    numberOfEvents: 32,
   }
 
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
+      const locationEvents = (location === 'all') ? events : events.filter((event) => event.location === location);
+      const shownEvents = locationEvents.slice(0, this.state.numberOfEvents);
       this.setState({
-        events: locationEvents
+        events: shownEvents,
+        currentLocation: location
       });
     });
   }
+
+  updateNumberOfEvents = (number) => {
+    const newNum = number;
+    this.setState({
+      numberOfEvents: newNum
+    });
+    this.updateEvents(this.state.currentLocation);
+  };
 
   componentDidMount() {
     this.mounted = true;
@@ -40,6 +50,7 @@ class App extends Component {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
         <EventList events={this.state.events} />
       </div>
     );
